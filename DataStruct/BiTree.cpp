12e76e99b5fbbed4ca2504cde6a28e86699b78bb
01 <01@DESKTOP-5ULK03K>
2021-07-15 16:27:59 +0800

    2021-07-15

--- DataStruct/BiTree.cpp
+++ DataStruct/BiTree.cpp
@@ -15,8 +15,8 @@
 // Copyright (c) 2011 rubicon IT GmbH
 #include "BiTree.h"
 #include "BitNode.h"
-
-
+#include <deque>
+#include <stack>
 
 BiTree::BiTree()
 {
@@ -29,10 +29,6 @@ void BiTree::visit(BitNode* T)
 	cout << " " << T->data << " ";
 }
 
-void BiTree::createBiTree(ElemType e)
-{
-
-}
 
 void BiTree::inOrder(BitNode* T)
 {
@@ -65,4 +61,120 @@ void BiTree::postOrder(BitNode* T)
 		BiTree::visit(T);
 		
 	}
+}
+
+
+
+BitNode* BiTree::setRoot(ElemType e)
+{
+	if (!root)
+	{
+		root = new BitNode(e);
+		root->data = e;
+		return root;
+
+	}
+	else
+	{
+		throw
+			"根已经存在";
+	}
+}
+
+
+
+BitNode* BiTree::getRoot()
+{
+	// TODO: 在此处插入 return 语句
+	if (root)
+	{
+		return this->root;
+	}
+	else
+	{
+		throw "root is null";
+	}
+	
+}
+
+BiTree& BiTree::getTree()
+{
+	return *this;
+}
+
+void BiTree::insertNewNode(ElemType e)
+{
+	if (!root)
+	{
+		throw "error";
+	}
+	deque<BitNode*> deq;
+	auto p = root;
+
+	deq.push_back(p);
+	while (deq.empty())
+	{
+		auto q = deq.front();
+		if (!q->lchild)
+		{
+			q->lchild = new BitNode(e);
+			break;
+		}
+
+		if (q->lchild)
+		{
+			deq.push_back(q->lchild);
+			if (!q->rchild)
+			{
+				q->rchild = new BitNode(e);
+				break;
+			}
+
+		}
+		else if(q->rchild)
+		{
+			deq.push_back(q->rchild);
+		}
+		
+		deq.pop_front();
+	}
+
+}
+
+int BiTree::getHigh(BitNode* T)
+{
+	if (!T)
+	{
+		return 0;
+	}
+	auto m = getHigh(T->lchild);
+	auto n= getHigh(T->rchild);
+	return m > n ? m + 1 : n + 1;
+}
+
+void BiTree::preOrdernorecursion(BitNode* T)
+{
+	if (!T)
+	{
+		throw "Error";
+
+	}
+
+	stack<BitNode*> STACK;
+	auto q = T;
+	STACK.push(q);
+	while (!STACK.empty()||q)
+	{
+		
+		while (q)
+		{
+			visit(q);
+			STACK.push(q);
+			q=q->lchild;
+		}
+		q= STACK.top();
+		q = q->lchild;
+		STACK.pop();
+	}
+	
 }
--- DataStruct/BiTree.h
+++ DataStruct/BiTree.h
@@ -19,11 +19,11 @@ class BiTree
 {
 
 public:
+
 	BiTree();
 	//访问当前节点
 	void visit(BitNode* T);
 
-	void createBiTree(ElemType e);
 	//前序遍历
 	void preOrder(BitNode*T);
 
@@ -34,7 +34,23 @@ public:
 	//后序遍历
 	void postOrder(BitNode* T);
 
+	//设置根节点
+	BitNode* setRoot(ElemType e);
 
+	//获取根
+	BitNode* getRoot();
+
+	//获取树
+	BiTree& getTree();
+
+	//插入新节点
+	void insertNewNode(ElemType e);
+
+	//获取树高
+	int getHigh(BitNode *T);
+
+	//前序遍历非递归
+	void preOrdernorecursion(BitNode* T);
 private:
 	BitNode* root;
 	int high;
